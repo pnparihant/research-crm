d1d60037d4ff10a6445552c44b6182482eece551
Prathamesh Pawar <prathmeshpawar4169@gmail.com>
2026-06-26 12:44:18 +0530

    Added next config file

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "standalone",
-    // disable google fonts fetching at build time
   env: {
     NEXT_PUBLIC_SKIP_GOOGLE_FONTS: "true",
   },
